7b098c598b01630c7e9af625b5d59d8f08c6e4af
Camilo318 <camiloa0318@gmail.com>
2020-11-08 17:20:32 -0500

    Add exchange section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Exchange from './Exchange'
 import Header from './Header'
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
     return (
         <>
             <Header />
+            <Exchange />
         </>
     )
 }
---
+++ src/components/Exchange.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import moduleName from '../assets/img/Bitcoin.svg'
+
+const Exchange = () => {
+    const date = new Date()
+    return (
+        <section className='exchange'>
+            <div className='exchange__img'></div>
+            <div className='exchange__info'>
+                <h2>
+                    Visualizamos todas las tasas de cambio.
+                </h2>
+                <p>
+                    Traemos información en tiempo real de las casas de cambio y las monedas más importantes del mundo.
+                </p>
+            </div>
+            <section className='exchange__tables'>
+                <h3>Monedas</h3>
+                <div className='exchange__grid'>
+                    <div className="grid-item left">Bitcoin</div>
+                    <div className="grid-item right">$ 1.96</div>
+                    <div className="grid-item left">Ethereum</div>
+                    <div className="grid-item right">$ 0.07</div>
+                    <div className="grid-item left">Ripple</div>
+                    <div className="grid-item right">$ 2.15</div>
+                    <div className="grid-item left">Stellar</div>
+                    <div className="grid-item right">$ 4.96</div>
+                </div>
+                <p>
+                    <strong>Actualizado: </strong>
+                    { date.toDateString() }
+                </p>
+            </section>
+        </section>
+    )
+}
+
+export default Exchange
